099ff1027e576dc7c5ee8cec7da28810b8763376
Guillaume EHRET <guillaume@dev-mind.fr>
2017-02-09 22:17:13 +0100

    first commit

---
+++ src/main/java/com/devmind/performance/Devoxx2017Application.java
@@ -0,0 +1,12 @@
+package com.devmind.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devoxx2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devoxx2017Application.class, args);
+	}
+}
---
+++ src/test/java/com/devmind/performance/Devoxx2017ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devmind.performance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Devoxx2017ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
